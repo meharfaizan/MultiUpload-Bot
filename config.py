e25b29f88496f2c5aa347c5db15270fd859e3da3
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-11 20:37:07 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
    # get a token from @BotFather
    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5502855202:AAGW3MYUov5Gb2wWgpcaNTgxDjZ98IFh4P8")
    # channel id = -100 (for logs)
-   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001378693486"))   
+   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001858175451"))   
    # Array to store users who are authorized to use the bot 
-   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "754495556").split())    
+   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1430593323").split())    
    # force sub user to the channel (without @)
    CHNAME = os.environ.get("CHNAME", "animecolony")
